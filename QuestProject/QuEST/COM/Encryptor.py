81b631f68f832c3ba79e4b9188b59bfe1b338829
jeeva91 <jee1199@gmail.com>
2017-05-01 20:07:30 -0400

    implemented twofish for encryption

--- QuestProject/QuEST/COM/Encryptor.py
+++ QuestProject/QuEST/COM/Encryptor.py
@@ -5,6 +5,7 @@ Created on Apr 29, 2017
 '''
 
 import pyaes
+from twofish import Twofish
 
 class Encryptor(object):
     '''
@@ -19,16 +20,27 @@ class Encryptor(object):
         self.key=key
         self.encoder=pyaes.AESModeOfOperationCTR(self.key)
         self.decoder=pyaes.AESModeOfOperationCTR(self.key)
+        self.tfh=Twofish(self.key)
         
-    def encode(self,string):
-        return self.encoder.encrypt(string)
+    def encode(self,message):
+        padded='{message: <16}'.format(message)
+        #return self.encoder.encrypt(string)
+        return self.tfh.encrypt(padded)
     
-    def decode(self,bytedata):
+    def decod(self,bytedata):
+        '''code for the aes not used
+        '''
         
         try:
-            return self.decoder.decrypt(bytedata).decode('utf-8')
+            print("inside decoder")
+            print(bytedata)
+            data= self.decoder.decrypt(bytedata)
+            print(data)
+            return data
         except:
             pass
         finally:
             return ""
+    def decode(self, bytedata):
         
+        return self.tfh.decrypt(bytedata)    
